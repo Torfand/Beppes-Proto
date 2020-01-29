b5837634edda824cdc8f3eb16ba382b413ff790a
Torfand <torfin@getacademy.no>
2020-01-29 15:44:25 +0100

    Update Model.js

--- js/Model.js
+++ js/Model.js
@@ -1,4 +1,3 @@
-
 let model;
 model = {
     menuItems: [
@@ -15,14 +14,8 @@ model = {
     calendarItems: [
         months = ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
         day = ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'],
-        daysInMonths = { January: 31, February: 28, March: 31, April: 30, May: 31, June: 30, July: 31, August: 31, September: 30, October: 31, November: 30, December: 31,},
+        daysInMonths = { January: 31, February: 28, March: 31, April: 30, May: 31, June: 30, July: 31, August: 31, September: 30, October: 31, November: 30, December: 31, },
 
     ]
 
 };
-
-
-
-
-
-
