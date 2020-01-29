21b19c460c0d3bf4a764484e8119042b2e120213
Torfand <torfin@getacademy.no>
2020-01-29 14:07:08 +0100

    123

--- js/Model.js
+++ js/Model.js
@@ -1,4 +1,21 @@
-const model = {
+const model;
 
-}
-HeipåDeg
+model = {
+    menuItems: [
+        { headerName: 'Navn' },
+        { createFunction: 'logout', buttonName: 'Logg ut' },
+        { createFunction: 'Calendar', buttonName: 'Min kalender' },
+        { createFunction: 'Tasks', buttonName: 'Oppgaver' },
+        { createFunction: 'Orders', buttonName: 'Bestillinger' },
+        { createFunction: 'Roster', buttonName: 'Vaktliste' },
+        { createFunction: 'Administrative', buttonName: 'Administrativt' },
+
+    ],
+
+    calendarItems: [
+        months = ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
+        day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    
+    ]
+
+};
