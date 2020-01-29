07bdbcd137625c030d3e03c805f9696d53c626b9
Torfand <torfin@getacademy.no>
2020-01-29 14:37:17 +0100

    Updates

--- js/Model.js
+++ js/Model.js
@@ -1,5 +1,5 @@
-const model;
 
+let model;
 model = {
     menuItems: [
         { headerName: 'Navn' },
@@ -14,9 +14,15 @@ model = {
 
     calendarItems: [
         months = ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
-        day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-
+        day = ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'],
+        daysInMonths = { January: 31, February: 28, March: 31, April: 30, May: 31, June: 30, July: 31, August: 31, September: 30, October: 31, November: 30, December: 31,},
 
     ]
 
 };
+
+
+
+
+
+
