a090b301f0d65da0ce91840d7f90c6d574bff345
Torfand <torfin@getacademy.no>
2020-01-30 10:14:39 +0100

    Update Model.js

--- js/Model.js
+++ js/Model.js
@@ -1,21 +1,22 @@
-let model;
-model = {
-    menuItems: [
-        { headerName: 'Navn' },
-        { createFunction: 'logout', buttonName: 'Logg ut' },
-        { createFunction: 'Calendar', buttonName: 'Min kalender' },
-        { createFunction: 'Tasks', buttonName: 'Oppgaver' },
-        { createFunction: 'Orders', buttonName: 'Bestillinger' },
-        { createFunction: 'Roster', buttonName: 'Vaktliste' },
-        { createFunction: 'Administrative', buttonName: 'Administrativt' },
+const model = {
+    admin: {
+        menuItems: [
+            
+            { createFunction: 'logout', buttonName: 'Logg ut' },
+            { createFunction: 'Calendar', buttonName: 'Min kalender' },
+            { createFunction: 'Tasks', buttonName: 'Oppgaver' },
+            { createFunction: 'Orders', buttonName: 'Bestillinger' },
+            { createFunction: 'Roster', buttonName: 'Vaktliste' },
+            { createFunction: 'Administrative', buttonName: 'Administrativt' },
 
-    ],
+        ],
 
-    calendarItems: [{
-        months: ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
-        day: ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'],
-        daysInMonths: { January: 31, February: 28, March: 31, April: 30, May: 31, June: 30, July: 31, August: 31, September: 30, October: 31, November: 30, December: 31, },
-        Notes = [''],
-    }]
+        calendarItems: [{
+            months: ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
+            day: ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'],
+            daysInMonths: { January: 31, February: 28, March: 31, April: 30, May: 31, June: 30, July: 31, August: 31, September: 30, October: 31, November: 30, December: 31, },
+            Notes: [''],
+        }]
+    }
 
 };
