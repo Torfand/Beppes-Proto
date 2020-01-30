c5f55c14eff43437176ec17cf7dca8cfd3f55014
Tom Erik Sortland <57662099+tomeriksortland@users.noreply.github.com>
2020-01-30 15:20:50 +0100

    Update Model.js

--- js/Model.js
+++ js/Model.js
@@ -5,21 +5,59 @@ const info = {
         { id: 2, name: 'Ansatt 1', role: 'Baker' },
         { id: 3, name: 'Ansatt 2', role: 'barista' },
         { id: 4, name: 'Ansatt 3', role: 'konditor' },
-        { name: 'Ansatt 4', role: 'vaskehjelp' },
-        { name: 'Ansatt 5', role: 'barista' },
-        { name: 'Ansatt 6', role: 'ekstrahjelp' },
+        { id: 5, name: 'Ansatt 4', role: 'vaskehjelp' },
+        { id: 6, name: 'Ansatt 5', role: 'barista' },
+        { id: 7, name: 'Ansatt 6', role: 'ekstrahjelp' },
     ],
 
     workHours: [
-        { hours: '04:00 - 10:00' },
-        { hours: '06:00 - 14:00' },
-        { hours: '07:00 - 15:00' },
-        { hours: '08:00 - 16:00' },
-        { hours: '09:00 - 17:00' },
-        { hours: '09:00 - 17:00' },
+        { id: 1, hours: '04:00 - 10:00' },
+        { id: 2, hours: '06:00 - 14:00' },
+        { id: 3, hours: '07:00 - 15:00' },
+        { id: 4, hours: '08:00 - 16:00' },
+        { id: 5, hours: '09:00 - 17:00' },
+        { id: 6, hours: '09:00 - 17:00' },
     ],
 
-    cakeTypes: {
+    
+
+    cakeTypes: [
+        { id: 1, cake: 'Bløtekake', fillings: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], base: [3] },
+        { id: 2, cake: 'Laktosefri Bløtekake', fillings: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], base: [3] },
+        { id: 3, cake: 'Sjokoladekake' },
+        { id: 4, cake: 'Moussekake' },
+        { id: 5, cake: 'Raw kake' },
+        { id: 6, cake: 'Ostekake' },
+        { id: 7, cake: 'Gulrotkake' },
+    ],
+
+    cakeFillings: [
+        { id: 1, fillings: 'Jordbær' },
+        { id: 2, fillings: 'Bringebær' },
+        { id: 3, fillings: 'Ananas' },
+        { id: 4, fillings: 'Aprikos' },
+        { id: 5, fillings: 'Bryllupsblanding' },
+        { id: 6, fillings: 'Eple' },
+        { id: 7, fillings: 'Vaniljekrem' },
+        { id: 8, fillings: 'Valnøtter' },
+        { id: 9, fillings: 'Mandler' },
+        { id: 10, fillings: 'Krokan' },
+        { id: 11, fillings: 'Banan' },
+        { id: 12, fillings: 'Rørte Bær' },
+        { id: 13, fillings: 'Bringebærmousse' },
+        { id: 14, fillings: 'Jordbærmousse' },
+        { id: 15, fillings: 'Sjokolademousse' },
+        { id: 16, fillings: 'Sjømannskake (Mandler & Ananas)' },
+    ],
+
+    cakeBase: [
+        { id: 1, base: 'Sjokoladebunn' },
+        { id: 2, base: 'Browniebunn' },
+        { id: 3, base: 'Sukkerbrødbunn' },
+        { id: 4, base: 'Nøttebunn' },
+        { id: 5, base: 'Sjokoladebunn Glutenfri' },
+        { id: 6, base: 'Standardbunn' },
+    ]
 
 
         // bløtkake: {
@@ -72,7 +110,7 @@ const info = {
 
 
 
-}
+
 
 
 
