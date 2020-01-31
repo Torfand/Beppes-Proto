abd12fa781e1c776da0b5574313983af8a280619
Tom Erik Sortland <57662099+tomeriksortland@users.noreply.github.com>
2020-01-31 11:45:15 +0100

    Update Model.js

--- js/Model.js
+++ js/Model.js
@@ -1,20 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const model = {
 
 
@@ -40,33 +24,95 @@ const model = {
 
 
     cakeTypes: [
-        { id: 1, cake: 'Bløtekake', fillings: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], base: [3] },
-        { id: 2, cake: 'Laktosefri Bløtekake', fillings: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], base: [3] },
-        { id: 3, cake: 'Sjokoladekake' },
-        { id: 4, cake: 'Moussekake' },
+        { id: 1, cake: 'Bløtekake Rund Med Marsipan', cakeFillings: [1, 2, 3, 4, 5, 6], cakeBase: [3], addOns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], cakeSize:[1,2], cakePrice:[1,2]  },
+        { id: 1, cake: 'Bløtekake Rund Uten Marsipan', cakeFillings: [1, 2, 3, 4, 5, 6], cakeBase: [3], addOns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], cakeSize:[1,2], cakePrice:[3,4] },
+        { id: 1, cake: 'Bløtekake Telegram Med Marsipan', cakeFillings: [1, 2, 3, 4, 5, 6], cakeBase: [3],addOns:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], cakeSize:[3,4,5], cakePrice:[5,6,7]},
+        { id: 1, cake: 'Bløtekake Telegram Uten Marsipan', cakeFillings: [1, 2, 3, 4, 5, 6], cakeBase: [3],addOns:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], cakeSize:[3,4,5], cakePrice:[8,9, 10] },
+        { id: 2, cake: 'Glutenfri Bløtekake', cakeFillings: [1, 2, 3, 4, 5, 6], cakeBase: [3], addOns:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], cakeSize:[1,2,4,5],cakePrice:[11,12,13,14] },
+        { id: 3, cake: 'Sjokoladekake Standard', addOns: [14, 4, 1, 15, 10, 16], cakeSize: [6, 7, 8, 9, 10], cakePrice: [15, 16, 17, 18, 19] },
+        { id: 3, cake: 'Sjokoladekake Sjokoladekrem', addOns: [14, 4, 1, 15, 10, 16], cakeSize: [6, 7, 8, 9, 10], cakePrice: [20, 21, 22, 23, 24] },
+        { id: 4, cake: 'Moussekake', cakeFillings: [1, 2,7], cakeBase:[1,2,3,4], cakeSize:[1,2,3], cakePrice:[] }, 
         { id: 5, cake: 'Raw kake' },
         { id: 6, cake: 'Ostekake' },
         { id: 7, cake: 'Gulrotkake' },
+
     ],
 
     cakeFillings: [
+
         { id: 1, fillings: 'Jordbær' },
         { id: 2, fillings: 'Bringebær' },
         { id: 3, fillings: 'Ananas' },
         { id: 4, fillings: 'Aprikos' },
         { id: 5, fillings: 'Bryllupsblanding' },
         { id: 6, fillings: 'Eple' },
-        { id: 7, fillings: 'Vaniljekrem' },
-        { id: 8, fillings: 'Valnøtter' },
-        { id: 9, fillings: 'Mandler' },
-        { id: 10, fillings: 'Krokan' },
-        { id: 11, fillings: 'Banan' },
-        { id: 12, fillings: 'Rørte Bær' },
-        { id: 13, fillings: 'Bringebærmousse' },
-        { id: 14, fillings: 'Jordbærmousse' },
-        { id: 15, fillings: 'Sjokolademousse' },
-        { id: 16, fillings: 'Sjømannskake (Mandler & Ananas)' },
+        { id: 7, fillings: 'Sjokolade' },
+
     ],
+    addOns: [
+        { id: 1, addon: 'Vanilje Krem', price: 75, },
+        { id: 2, addon: 'Krokan', price: 75, },
+        { id: 3, addon: 'Hvalnøtter', price: 75, },
+        { id: 4, addon: 'Banan', price: 75, },
+        { id: 5, addon: 'Mandler', price: 75, },
+        { id: 6, addon: 'Rørte Bær', price: 75, },
+        { id: 7, addon: 'Laktosefri', price: 75, },
+        { id: 8, addon: 'Oppdelt Kake', price: 75, },
+        { id: 9, addon: 'Jordbærmousse', price: 98, },
+        { id: 10, addon: 'Bringebærmousse', price: 98, },
+        { id: 11, addon: 'Sjokolademousse', price: 98, },
+        { id: 12, addon: 'Bilde', price: 220, },
+        { id: 13, addon: 'Text', price: 0, },
+        { id: 14, addon: 'Bringebær', price: 75, },
+        { id: 15, addon: 'Ostekrem', price: 75, },
+        { id: 16, addon: 'Oreokrem', price: 98, },
+    ],
+
+    cakeSize: [
+        { id: 1, size: '6-8 Pers' },
+        { id: 2, size: '10 - 12 Pers' },
+        { id: 3, size: '12 - 15 Pers' },
+        { id: 4, size: '16 - 23 Pers' },
+        { id: 5, size: '24 - 30 Pers' },
+        { id: 6, size: '10 Pers' },
+        { id: 7, size: '15 Pers' },
+        { id: 8, size: '20 Pers' },
+        { id: 9, size: '30 Pers' },
+        { id: 10, size: '40 Pers' },
+    ],
+
+
+    cakePrice: [
+        { id: 1, price: 360, notes: 'bløtekake 6-8 med Marsipan' },
+        { id: 2, price: 595, notes: 'bløtekake 10-12 med Marsipan' },
+        { id: 3, price: 310, notes: 'Bløtekake 6-8 Uten Marsipan' },
+        { id: 4, price: 575, notes: 'Bløtekake 10-12 Uten Marsipan' },
+        { id: 5, price: 735, notes: 'Telegramkake 10-15 Med Marsipan' },
+        { id: 6, price: 895, notes: 'Telegramkake 16-23 Med Marsipan' },
+        { id: 7, price: 998, notes: 'Telegramkake 24-30 Med Marsipan' },
+        { id: 8, price: 669, notes: 'Telegramkake 12-15 Uten Marsipan' },
+        { id: 9, price: 834, notes: 'Telegramkake 16-23 Uten Marsipan' },
+        { id: 10, price: 941, notes: 'Telgramkake 24-30 Uten Marsipan' },
+        { id: 11, price: 410, notes: 'Glutenfribløtekake 6-8' },
+        { id: 12, price: 695, notes: 'Glutenfribløtekake 10-12' },
+        { id: 13, price: 995, notes: 'Glutenfribløtekake 16-23' },
+        { id: 14, price: 1098, notes: 'Glutenfribløtekake 24-30' },
+        { id: 15, price: 390, notes: 'Sjoko Standard 10 ' },
+        { id: 16, price: 495, notes: 'Sjoko Standard 15' },
+        { id: 17, price: 600, notes: 'Sjoko Standard 20' },
+        { id: 18, price: 890, notes: 'Sjoko Standard 30' },
+        { id: 19, price: 1190, notes: 'Sjoko Standard 40' },
+        { id: 20, price: 465, notes: 'Sjoko Krem 10' },
+        { id: 21, price: 570, notes: 'Sjoko Krem 15' },
+        { id: 22, price: 675, notes: 'Sjoko Krem 20' },
+        { id: 23, price: 965, notes: 'Sjoko Krem 30' },
+        { id: 24, price: 1265, notes: 'Sjoko Krem 40' },
+        { id: 25, price: 472, notes: 'Mousse 6-8' },
+        { id: 26, price: 708, notes: 'Mousse 10-12' },
+        { id: 27, price: 885, notes: 'SMousse 12-15' },
+
+    ],
+
 
     cakeBase: [
         { id: 1, base: 'Sjokoladebunn' },
@@ -95,10 +141,11 @@ const model = {
             { createFunction: 'Billing', buttonName: 'Til Fakturering' },
             { createFunction: 'ApprovedOrders', buttonName: 'Godkjente Bestillinger' },
             { createFunction: 'CanceledOrders', buttonName: 'Kanselerte Bestillinger' },
-           
+
 
         ],
-        
+
+
         calendarItems: [{
             months: ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
             day: ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'],
